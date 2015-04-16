dfe73c8c512d205a80b65ab43b7d01f6a8228e4c
Mikeqin <Fengling.Qin@gmail.com>
2015-04-16 18:01:47 +0800

    Firmware: Add more functions to sha2

--- firmware/code/inc/sha2.h
+++ firmware/code/inc/sha2.h
@@ -64,7 +64,21 @@
            | ((uint32_t) *((str) + 0) << 24);   \
 }
 
+typedef struct {
+    unsigned int tot_len;
+    unsigned int len;
+    unsigned char block[2 * SHA256_BLOCK_SIZE];
+    uint32_t h[8];
+} sha256_ctx;
+
 extern uint32_t sha256_k[64];
+
+void sha256_init(sha256_ctx * ctx);
+void sha256_update(sha256_ctx *ctx, const unsigned char *message,
+                   unsigned int len);
+void sha256_final(sha256_ctx *ctx, unsigned char *digest);
+void sha256(const unsigned char *message, unsigned int len,
+            unsigned char *digest);
 
 void data_pkg(const uint8_t *data, uint8_t *out);
 void data_convert(uint8_t *data);
