8b7ad174686672fe7bf118bb1bf1b92a91edafc0
archang <archang@canaan-creative.com>
2015-04-23 08:43:48 +0800

    APP: Cleanup

--- app/script.js
+++ app/script.js
@@ -22,7 +22,6 @@ chrome.hid.onDeviceRemoved.addListener(function(deviceId) {
 
 function main() {
 	setInterval(function() {
-		
 		var xhr = new XMLHttpRequest();
 		xhr.open("GET", 'https://data.btcchina.com/data/ticker?maket=all', true);
 
@@ -33,21 +32,19 @@ function main() {
 			}
 		};
 		xhr.send();
-
-	} , 1000*5);
+	}, 1000*5);
 
 	setInterval(function(){
-		chrome.storage.local.get('pool' , function(result){
-			if(typeof(result.pool) != "undefined"){
+		chrome.storage.local.get('pool' , function(result) {
+			if (result.pool !== undefined) {
 				pool_address = result.pool.address;
 				pool_worker= result.pool.worker;
 				pool_password = '123456';
 				$.httpRequest._connect(pool_worker, pool_password , pool_address);
-			}else{
+			} else
 				console.error('Pleace set pool');		
-			}
 		});
-	} , 1000);
+	}, 1000);
 
 	setInterval(function() {
 		var data = nanos[0].get_data();
@@ -56,16 +53,16 @@ function main() {
 			if (data.type === P_NONCE) {
 				var nonce = data.nonce.toString(16);
 				var pad = "00000000";
-				var nonce = pad.substring(0, pad.length - nonce.length) + nonce;
+				nonce = pad.substring(0, pad.length - nonce.length) + nonce;
 				console.log("Nonce : ", nonce );
 				nonce_list.push(nonce);
 					
 				new_nonce = '';
-				new_nonce = nonce.substr(6,2)+nonce.substr(4,2)+nonce.substr(2,2)+nonce.substr(0,2);
-				var solution = work_data.slice(0,152)+new_nonce+work_data.slice(160,256);
-				$.httpRequest._connect(pool_worker, pool_password , pool_address , solution);
-			}
-			
+				new_nonce = nonce.substr(6, 2) + nonce.substr(4, 2) + nonce.substr(2, 2) + nonce.substr(0, 2);
+				var solution = work_data.slice(0, 152) + new_nonce + work_data.slice(160, 256);
+				$.httpRequest._connect(pool_worker, pool_password, pool_address, solution);
+			} else if (data.type === P_STATUS)
+				MHz = data.frequency;
 		}
 	}, 1000);
 
@@ -86,7 +83,7 @@ function main() {
 }
 
 jQuery.httpRequest = {
-	_connect : function (username , password ,  url ,data) {
+	_connect: function(username, password, url, data) {
 		if(!username && !password && !url)
 			return -1;
 
@@ -100,34 +97,32 @@ jQuery.httpRequest = {
 			'method' : method , 
 			'id' : 10 , 
 			'params' : params
-		}
+		};
 		obj = JSON.stringify(obj);
 	
 		var xhr = new XMLHttpRequest();
 		xhr.open("POST", url, true);
-		xhr.setRequestHeader("Authorization" , authhdr );
-		xhr.setRequestHeader("Content-type" , "application/json");
+		xhr.setRequestHeader("Authorization", authhdr);
+		xhr.setRequestHeader("Content-type", "application/json");
 
 		xhr.onreadystatechange = function() {
-			if (xhr.readyState == 4) {
+			if (xhr.readyState === 4) {
 				message = JSON.parse(xhr.responseText);
-				if(message.error == null){
-					if((typeof(message.result.data) != 'undefined') && (typeof(message.result.midstate) != 'undefined')){
+				if (message.error === null) {
+					if ((message.result.data !== undefined) && (message.result.midstate !== undefined)) {
 						var raw = gw_pool2raw(message.result.midstate, message.result.data);
 						work_list.push(message.result.data);
 						nanos[0].push_data(raw);
-					}else{
+					} else {
 						var nonce = nonce_list.shift();
 						$("#debug-info").val('Subminting ' + nonce+"\r\n"+$("#debug-info").val());
 					}
-				}else{
+				} else
 					console.error(message.error);
-				}
 			}
 		};
 		xhr.send(obj);
 	}	
-
 };
 
 var test = {
@@ -169,14 +164,14 @@ var TEST = [
 ];
 
 var TEST2 = [
-    0xf6, 0xcd, 0x53, 0x45, 0xf8, 0xde, 0x06, 0x4e,
-    0xc4, 0xb0, 0x40, 0x1a, 0x2f, 0xdf, 0x2b, 0x84,
-    0x50, 0xe1, 0x23, 0xa1, 0x7c, 0x25, 0xbf, 0xf6,
-    0x91, 0xdc, 0x24, 0x6c, 0x50, 0xe7, 0x15, 0x5e,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x71, 0x3c, 0x16, 0x18,
-    0xce, 0x78, 0x30, 0x55, 0x2c, 0x51, 0xfb, 0xb8
+	0xf6, 0xcd, 0x53, 0x45, 0xf8, 0xde, 0x06, 0x4e,
+	0xc4, 0xb0, 0x40, 0x1a, 0x2f, 0xdf, 0x2b, 0x84,
+	0x50, 0xe1, 0x23, 0xa1, 0x7c, 0x25, 0xbf, 0xf6,
+	0x91, 0xdc, 0x24, 0x6c, 0x50, 0xe7, 0x15, 0x5e,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x00, 0x00, 0x00, 0x00, 0x71, 0x3c, 0x16, 0x18,
+	0xce, 0x78, 0x30, 0x55, 0x2c, 0x51, 0xfb, 0xb8
 ];
 
 var raw = gw_pool2raw(test.midstat, test.data);
@@ -188,3 +183,6 @@ var nonce_list = [];
 var MHz = 0;
 init(main);
 //getWork();
+//
+//var pool = new Pool('stratum.btcchina.com', 3333, "canaan.apptest", "1234");
+//pool.run();
