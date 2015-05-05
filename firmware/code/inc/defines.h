eb167db33b6c8a8f8142343c83a39bbfa25e8279
Mikeqin <Fengling.Qin@gmail.com>
2015-05-05 16:44:09 +0800

    Firmware: Add shifter functions

--- firmware/code/inc/defines.h
+++ firmware/code/inc/defines.h
@@ -9,6 +9,7 @@
 #define __DEFINES_H_
 
 #define ASIC_COUNT		4
+#define ASIC_0V                 0 /* TODO: 0V */
 
 #endif /* __DEFINES_H_ */
 
---
+++ firmware/code/inc/libfunctions.h
@@ -0,0 +1,12 @@
+/*
+ * @brief libfunctions head file
+ *
+ * @note
+ *
+ * @par
+ */
+#ifndef __LIB_FUNCTIONS_H_
+#define __LIB_FUNCTIONS_H_
+
+void delay(unsigned int ms);
+#endif /* __LIB_FUNCTIONS_H_ */
