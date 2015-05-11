31473e1193aa93acc620021c1c2ab627a40e5bb7
Xiangfu <xiangfu@openmobilefree.net>
2015-05-11 13:57:53 +0800

    Add verbosity control

--- firmware/mini/make/defaultc.mk
+++ firmware/mini/make/defaultc.mk
@@ -12,12 +12,30 @@
 # For details see the UNLICENSE file at the root of the source tree.
 #
 
+SHELL = /bin/bash
+
 CROSS_COMPILE ?= arm-none-eabi-
 
 CC		:= $(CROSS_COMPILE)gcc
 LD		:= $(CROSS_COMPILE)ld
 SIZE	:= $(CROSS_COMPILE)size
 AR		:= $(CROSS_COMPILE)ar
+
+# ----- Verbosity control -----------------------------------------------------
+
+CC_normal       := $(CC)
+CPP_normal      := $(CPP)
+DEPEND_normal   = $(CPP_normal) $(CFLAGS) -MM -MG
+
+ifeq ($(V),1)
+    CC          = $(CC_normal)
+    BUILD       =
+    DEPEND      = $(DEPEND_normal)
+else
+    CC          = @echo "  CC       " $@ && $(CC_normal)
+    BUILD       = @echo "  BUILD    " $@ &&
+    DEPEND      = @$(DEPEND_normal)
+endif
 
 LIBNAME	 = $(shell pwd |sed 's/^\(.*\)[/]//' )
 
