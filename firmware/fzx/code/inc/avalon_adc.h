a0ff15683a6275286f9a814af784743a5798e204
Johnson-Fan <fanzixiao@canaan-creative.com>
2016-06-14 18:45:11 +0800

    FZX: Modify ADC

--- firmware/fzx/code/inc/avalon_adc.h
+++ firmware/fzx/code/inc/avalon_adc.h
@@ -10,12 +10,15 @@
 
 #include "adc_11xx.h"
 
-#define ADC_PORT      0
-#define ADC_VCORE_PIN 13
-#define ADC_V12V_PIN  12
+#define ADC_CHANNEL_VBASE   (ADC_CH7)
+#define ADC_CHANNEL_NTC1    (ADC_CH3)
+#define ADC_CHANNEL_NTC2    (ADC_CH6)
+#define ADC_CHANNEL_V12V_1  (ADC_CH5)
+#define ADC_CHANNEL_V12V_2  (ADC_CH0)
+#define ADC_CHANNEL_VCORE1  (ADC_CH2)
+#define ADC_CHANNEL_VCORE2  (ADC_CH1)
 
-#define ADC_CHANNEL_V12V  (ADC_CH1)
-#define ADC_CHANNEL_VCORE (ADC_CH2)
+#define ADC_CAPCPUNT    7
 
 void adc_init(void);
 void adc_read(uint8_t channel, uint16_t *data);
--- firmware/fzx/code/src/avalon_adc.c
+++ firmware/fzx/code/src/avalon_adc.c
@@ -11,12 +11,32 @@
 #include "board.h"
 #include "avalon_adc.h"
 
+#define ADC_VBASE_PORT    0
+#define ADC_VBASE_PIN     23
+#define ADC_NTC1_PORT     0
+#define ADC_NTC1_PIN      14
+#define ADC_NTC2_PORT     0
+#define ADC_NTC2_PIN      22
+#define ADC_V12V_1_PORT   0
+#define ADC_V12V_1_PIN    16
+#define ADC_V12V_2_PORT   0
+#define ADC_V12V_2_PIN    11
+#define ADC_VCORE1_PORT   0
+#define ADC_VCORE1_PIN    13
+#define ADC_VCORE2_PORT   0
+#define ADC_VCORE2_PIN    12
+
 void adc_init(void)
 {
 	ADC_CLOCK_SETUP_T ADCSetup;
 
-	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_PORT, ADC_VCORE_PIN, FUNC2);
-	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_PORT, ADC_V12V_PIN, FUNC2);
+	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_VBASE_PORT, ADC_VBASE_PIN, FUNC1);
+	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_NTC1_PORT, ADC_NTC1_PIN, FUNC2);
+	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_NTC2_PORT, ADC_NTC2_PIN, FUNC1);
+	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_V12V_1_PORT, ADC_V12V_1_PIN, FUNC1);
+	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_V12V_2_PORT, ADC_V12V_2_PIN, FUNC2);
+	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_VCORE1_PORT, ADC_VCORE1_PIN, FUNC2);
+	Chip_IOCON_PinMuxSet(LPC_IOCON, ADC_VCORE2_PORT, ADC_VCORE2_PIN, FUNC2);
 	Chip_ADC_Init(LPC_ADC, &ADCSetup);
 }
 
@@ -26,9 +46,9 @@ void adc_read(uint8_t channel, uint16_t *data)
 	/* Start A/D conversion */
 	Chip_ADC_SetStartMode(LPC_ADC, ADC_START_NOW, ADC_TRIGGERMODE_RISING);
 	/* Waiting for A/D conversion complete */
-	while (Chip_ADC_ReadStatus(LPC_ADC, (ADC_CHANNEL_T)channel, ADC_DR_DONE_STAT) != SET);
+	while (Chip_ADC_ReadStatus(LPC_ADC, (ADC_CHANNEL_T)channel, ADC_DR_DONE_STAT) != SET)
+		;
 	/* Read ADC value */
 	Chip_ADC_ReadValue(LPC_ADC, channel, data);
 	Chip_ADC_EnableChannel(LPC_ADC, (ADC_CHANNEL_T)channel, DISABLE);
 }
-
