c9642d238e1b8ef6c06ae5b589ed35f53bfa7b16
Johnson-Fan <fanzixiao@canaan-creative.com>
2016-06-14 19:17:49 +0800

    FZX: Modify led

--- firmware/fzx/code/inc/avalon_led.h
+++ firmware/fzx/code/inc/avalon_led.h
@@ -8,8 +8,15 @@
 #ifndef __AVALON_LED_H_
 #define __AVALON_LED_H_
 
+#define LED_ON      1
+#define LED_OFF     0
+
+#define LED_12V_1T  0x01
+#define LED_12V_1F  0x02
+#define LED_12V_2T  0x03
+#define LED_12V_2F  0x04
+
 void led_init(void);
-void led_set(uint8_t led_data);
-void test_io_init(void);
+void led_set(unsigned int led, unsigned int state);
 
 #endif /* __AVALON_LED_H_ */
