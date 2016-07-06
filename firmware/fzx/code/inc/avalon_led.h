3e3963fd4ad091638b63140772e9fcd6e4fe1ef7
Johnson-Fan <fanzixiao@canaan-creative.com>
2016-07-06 19:02:44 +0800

    FZX: fix led

--- firmware/fzx/code/inc/avalon_led.h
+++ firmware/fzx/code/inc/avalon_led.h
@@ -11,17 +11,19 @@
 #define LED_ON      1
 #define LED_OFF     0
 
-#define LED_12V     0x00
 #define LED_12V_1T  0x01
 #define LED_12V_1F  0x02
 #define LED_12V_2T  0x04
 #define LED_12V_2F  0x08
+#define LED_12V_1TF 0x10
+#define LED_12V_2TF 0x20
 
-#define TIMER_LED_12V    TIMER_ID3
-#define TIMER_LED_12V_1T TIMER_ID4
-#define TIMER_LED_12V_1F TIMER_ID5
-#define TIMER_LED_12V_2T TIMER_ID6
-#define TIMER_LED_12V_2F TIMER_ID7
+#define TIMER_LED_12V_1T  TIMER_ID3
+#define TIMER_LED_12V_1F  TIMER_ID4
+#define TIMER_LED_12V_2T  TIMER_ID5
+#define TIMER_LED_12V_2F  TIMER_ID6
+#define TIMER_LED_12V_1TF TIMER_ID7
+#define TIMER_LED_12V_2TF TIMER_ID8
 
 void led_init(void);
 void led_rgb(unsigned int rgb, unsigned int state);
