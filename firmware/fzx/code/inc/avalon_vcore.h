570444651aa7b7e37e2a2a57160fad46e24f414f
Johnson-Fan <fanzixiao@canaan-creative.com>
2016-07-06 17:03:06 +0800

    FZX: add read pg signal

--- firmware/fzx/code/inc/avalon_vcore.h
+++ firmware/fzx/code/inc/avalon_vcore.h
@@ -11,11 +11,15 @@
 #define VCORE1    1
 #define VCORE2    2
 
+#define PG1       0x01
+#define PG2       0x02
+
 void vcore_init(void);
 uint8_t  set_voltage(uint16_t vol);
 uint16_t get_voltage(void);
 void vcore_disable(uint8_t num);
 void vcore_enable(uint8_t num);
 void vcore_detect(void);
+uint8_t get_pg_flag(void);
 
 #endif /* __AVALON_VCORE_H_ */
